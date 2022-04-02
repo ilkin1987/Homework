31ba7193b118e4644883e294b63d889051c27d01
ilkin1987 <101628203+ilkin1987@users.noreply.github.com>
2022-04-02 06:39:32 -0700

    Homework

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            #region task1
+            
+            //int a = 5;
+            //int b = 8;
+            //int c = 10;
+
+            //if (a > b && a > c)
+            //{
+            //    Console.WriteLine($" en boyuk = (a)");
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    Console.WriteLine($" en boyuk = (b)");
+            //}
+            //else if (c > a && c > b)
+            //{
+            //    Console.WriteLine($"en boyuk = (c)");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ededler beraberdir");
+                #endregion
+            }
+            #region task2
+            //        static void ikinci()
+            //{
+            //        Console.WriteLine("birinci ededi daxil edin");
+            //        string a = Console.ReadLine();
+            //        int nums = int.Parse(a);
+            //    {
+            //        Console.WriteLine("ikinci ededi daxil edin");
+            //        string b = Console.ReadLine();
+            //        int nums2 = int.Parse(b);
+            //        {
+            //        Console.WriteLine("ucuncu ededi daxil edin");
+            //        string c = Console.ReadLine();
+            //        int nums3 = int.Parse(c);
+            //            {
+            //        Console.WriteLine("dorduncu ededi daxil edin");
+            //        string d = Console.ReadLine();
+            //        int nums4 = int.Parse(d);
+            //                {
+            //         Console.WriteLine("besinci ededi daxil edin");
+            //         string e = Console.ReadLine();
+            //         int nums5 = int.Parse(e);
+            //         int result = 0;
+            //         result = (nums + nums2 + nums3 + nums4 + nums5)/5;
+            //         Console.WriteLine(result);
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+        }
+    }
+
+
+
+
+        
+    
+
